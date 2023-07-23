3894621fc2dc920f34e768032f96b15b25f0978b
Seniru Senarathne <senirusenarathne@gmail.com>
2023-07-23 16:27:59 +0530

    Added files via upload

---
+++ comments.js
@@ -0,0 +1,24 @@
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("country");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("col-75");
+    a = div.getElementsByTagName("option");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+  //Java script function Initiated from w3schools
+
+  //Opening Thank you box
+
+
+  function openBox(){
+    elem = document.getElementById('popup1');
+  }
+  
---
+++ index.js
@@ -0,0 +1,40 @@
+function moveClouds(callback) {
+    /* clouds 1 & 2 move to the left 
+       clouds 3 & 4 to the right */
+    for (var i = 1; i < 5; i++) {
+      var cloud = document.getElementById("cloud" + i);
+      cloud.style.transitionTimingFunction = "ease-out";
+      cloud.style.transitionDuration = "10ms";
+      var top = window.getComputedStyle(cloud, null).getPropertyValue("top");
+  
+      var topValue = parseInt(top);
+      topValue = topValue - 20;
+      top = topValue + "px";
+  
+      cloud.style.top = top;
+  
+      var left = window.getComputedStyle(cloud, null).getPropertyValue("left");
+      var leftValue = parseInt(left);
+  
+      if (i < 3) {
+        leftValue = leftValue - 30;
+      } else {
+        leftValue = leftValue + 30;
+      }
+      left = leftValue + "px";
+  
+      cloud.style.left = left;
+    }
+  
+    // Delay the execution of the callback function after the transition completes
+    setTimeout(callback, 10);
+  }
+  
+  // Example usage:
+  moveClouds(function() {
+    // Code to execute after all clouds have disappeared
+    var textElement = document.getElementById("mainText");
+    textElement.innerText = "Welcome";
+  });
+
+  //JS part got from -  https://codepen.io/trixie13/pen/GNJqBJ
